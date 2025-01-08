165f05953e421e107e8e46f6f9110e66f47f01ff
Romanschek, Enrico <85785544+mezumroka@users.noreply.github.com>
2025-01-08 22:10:16 +0100

    Zwischenstand

--- egbt22lib/Convert.cs
+++ egbt22lib/Convert.cs
@@ -17,10 +17,42 @@ namespace egbt22lib
 
 
         public static readonly Srs DBRef_GK5 = Srs.Get(5685);
+        public static readonly Srs DBRef_Geoc = Srs.Get(5828);
 
         public static readonly Srs ETRS89_Geod_3D = Srs.Get(4937);
+        public static readonly Srs ETRS89_Geoc = Srs.Get(4936);
 
         public static readonly Srs EGBT22_Local = Srs.GetLocalTMSystemWithScale(4258, 50.8247, 13.9027, 1.0000346, 10000, 50000); // EGBT_LDP
+
+        public const string DBRef_GK5_to_EGBT22_Local_options = "+proj=pipeline " +
+            "+step +inv +proj=tmerc +lat_0=0 +lon_0=15 +k=1 +x_0=5500000 +y_0=0 +ellps=bessel " +
+            "+step +proj=cart +ellps=bessel " +
+            "+step +proj=helmert +x=584.9636 +y=107.7175 +z=413.8067 +rx=-1.1155 +ry=-0.2824 +rz=3.1384 +s=7.9922 +exact +convention=coordinate_frame " +
+            "+step +inv +proj=cart +ellps=GRS80 " +
+            "+step +proj=tmerc +lat_0=50.8247 +lon_0=13.9027 +k=1.0000346 +x_0=10000 +y_0=50000 +ellps=GRS80";
+
+        public const string DBRef_GK5_to_ETRS89_Geod_3D_options = "+proj=pipeline " +
+            "+step +inv +proj=tmerc +lat_0=0 +lon_0=15 +k=1 +x_0=5500000 +y_0=0 +ellps=bessel " +
+            "+step +proj=cart +ellps=bessel " +
+            "+step +proj=helmert +x=584.9636 +y=107.7175 +z=413.8067 +rx=-1.1155 +ry=-0.2824 +rz=3.1384 +s=7.9922 +exact +convention=coordinate_frame " +
+            "+step +inv +proj=cart +ellps=GRS80";
+
+        public const string EGBT22_Local_to_DBRef_GK5_options = "+proj=pipeline " +
+            "+step +inv +proj=tmerc +lat_0=50.8247 +lon_0=13.9027 +k=1.0000346 +x_0=10000 +y_0=50000 +ellps=GRS80 " +
+            "+step +proj=cart +ellps=GRS80 " +
+            "+step +inv +proj=helmert +x=584.9636 +y=107.7175 +z=413.8067 +rx=-1.1155 +ry=-0.2824 +rz=3.1384 +s=7.9922 +exact +convention=coordinate_frame " +
+            "+step +inv +proj=cart +ellps=bessel " +
+            "+step +proj=tmerc +lat_0=0 +lon_0=15 +k=1 +x_0=5500000 +y_0=0 +ellps=bessel";
+ 
+        public const string EGBT22_Local_to_ETRS89_Geod_3D_options = "+proj=pipeline " +
+            "+step +inv +proj=tmerc +lat_0=50.8247 +lon_0=13.9027 +k=1.0000346 +x_0=10000 +y_0=50000 +ellps=GRS80 +xy_out=deg";
+        
+        public const string ETRS89_Geod_3D_to_DBRef_GK5_options = "+proj=pipeline " +
+            "+step +proj=cart +ellps=GRS80 " +
+            "+step +inv +proj=helmert +x=584.9636 +y=107.7175 +z=413.8067 +rx=-1.1155 +ry=-0.2824 +rz=3.1384 +s=7.9922 +exact +convention=coordinate_frame " +
+            "+step +inv +proj=cart +ellps=bessel " +
+            "+step +proj=tmerc +lat_0=0 +lon_0=15 +k=1 +x_0=5500000 +y_0=0 +ellps=bessel";
+
 
         public static bool DBRef_GK5_to_EGBT22_Local(double[] gk5Rechts, double[] gk5Hoch, double[] h, out double[] localRechts, out double[] localHoch)
         {
@@ -31,8 +63,16 @@ namespace egbt22lib
                 localHoch = Array.Empty<double>();
                 return false;
             }
+            var (etrs89Lat, etrs89Lon, hell_) = Srs.Convert(gk5Rechts, gk5Hoch, h, DBRef_GK5, ETRS89_Geod_3D, DBRef_GK5_to_ETRS89_Geod_3D_options);
+            var geoid = Geoid.GetBKGBinaryGeoidHeights("GCG2016v2023", etrs89Lat, etrs89Lon);
+            var hell = new double[geoid.Length];
+            for (int i = 0; i < hell.Length; i++)
+                hell[i] = geoid[i] + h[i];
+            var (_, _, hellDBRef) = Srs.Convert(etrs89Lat, etrs89Lon, hell, ETRS89_Geod_3D, DBRef_GK5, ETRS89_Geod_3D_to_DBRef_GK5_options);
+            (localRechts, localHoch, _) = Srs.Convert(gk5Rechts, gk5Hoch, hellDBRef, DBRef_GK5, EGBT22_Local, DBRef_GK5_to_EGBT22_Local_options);
 
-            (localRechts, localHoch, _) = Srs.Convert(gk5Rechts, gk5Hoch, h, DBRef_GK5, EGBT22_Local);
+            //(localRechts, localHoch, _) = Srs.Convert(gk5Rechts, gk5Hoch, h, DBRef_GK5, EGBT22_Local);
+
             _logger?.LogInformation("Conversion from DBRef_GK5 to EGBT22_Local successful.");
             return true;
         }
@@ -46,12 +86,12 @@ namespace egbt22lib
                 gk5Hoch = Array.Empty<double>();
                 return false;
             }
-            var (etrs89Lat, etrs89Lon) = Srs.Convert(localRechts, localHoch, EGBT22_Local, ETRS89_Geod_3D);
-            var geoid = Geoid.GetBKGBinaryGeoidHeights("GCG2016v2023_NO", etrs89Lat, etrs89Lon);
+            var (etrs89Lat, etrs89Lon, _) = Srs.Convert(localRechts, localHoch, h, EGBT22_Local, ETRS89_Geod_3D);
+            var geoid = Geoid.GetBKGBinaryGeoidHeights("GCG2016v2023", etrs89Lat, etrs89Lon);
             var hell = new double[geoid.Length];
             for (int i = 0; i < hell.Length; i++)
                 hell[i] = geoid[i] + h[i];
-            (gk5Rechts, gk5Hoch, _) = Srs.Convert(etrs89Lat, etrs89Lon, hell, ETRS89_Geod_3D, DBRef_GK5);
+            (gk5Rechts, gk5Hoch, _) = Srs.Convert(localRechts, localHoch, hell, EGBT22_Local, DBRef_GK5, EGBT22_Local_to_DBRef_GK5_options);
             _logger?.LogInformation("Conversion from EGBT22_Local to DBRef_GK5 successful.");
             return true;
         }
---
+++ egbt22lib/Transformation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace egbt22lib
+{
+    public static class Transformation
+    {
+        public const double Sec2Rad = 4.8481368110953599358991410235795e-6;
+
+        public static readonly double[] Param_ETRS892DBRef =
+        { -584.9567,
+           - 107.7277,
+           - 413.8036,
+           1.1155257601,
+           0.2824170155,
+           - 3.1384505907,
+           - 7.992171 };
+
+        public static readonly double[] Param_DBRef2ETRS89 =
+        { 584.9636,
+           107.7175,
+           413.8067,
+           - 1.1155214628,
+           - 0.2824339890,
+           3.1384490633,
+           7.992235 };
+
+        private static readonly double[,] e2d, d2e;
+
+        static Transformation()
+        {
+            e2d = toRotationMatrix(Param_ETRS892DBRef);
+            d2e = toRotationMatrix(Param_DBRef2ETRS89);
+        }
+
+        private static double[,] toRotationMatrix(in double[] m)
+        {
+            double rX = m[3] * Sec2Rad;
+            double rY = m[4] * Sec2Rad;
+            double rZ = m[5] * Sec2Rad;
+            double cx = Math.Cos(rX);
+            double sx = Math.Sin(rX);
+            double cy = Math.Cos(rY);
+            double sy = Math.Sin(rY);
+            double cz = Math.Cos(rZ);
+            double sz = Math.Sin(rZ);
+
+            return new double[,]
+            {
+                {  cy * cz, (cx * sz) + (sx * sy * cz), (sx * sz) - (cx * sy * cz), m[0] },
+                { -cy * sz, (cx * cz) - (sx * sy * sz), (sx * cz) + (cx * sy * sz), m[1] },
+                {       sy,                   -sx * cy,                     cx* cy, m[2] },
+                {      0.0,                        0.0,                        0.0, 1.0 + (m[6] * 1.0e-6) }
+            };
+        }
+
+        private static Point rotateByMatrix(in double[,] m, in Point p)
+        {
+            return new Point(
+                (m[3, 3] * ((m[0, 0] * p.X) + (m[0, 1] * p.Y) + (m[0, 2] * p.Z))) + m[0, 3],
+                (m[3, 3] * ((m[1, 0] * p.X) + (m[1, 1] * p.Y) + (m[1, 2] * p.Z))) + m[1, 3],
+                (m[3, 3] * ((m[2, 0] * p.X) + (m[2, 1] * p.Y) + (m[2, 2] * p.Z))) + m[2, 3]);
+        }
+
+        public static Point DbrefToEtrs89(in Point p)
+        {
+            return rotateByMatrix(d2e, p);
+        }
+
+        public static Point Etrs89ToDbref(in Point p)
+        {
+            return rotateByMatrix(e2d, p);
+        }
+
+
+
+    }
+}
